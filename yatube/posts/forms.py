2a4faa26613cc334ab850d656e9f9a349d8aa3a8
Руслан Трофимов <edmondkoko777@gmail.com>
2022-10-30 16:01:05 +0300

    Финальный проект спринта 4

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
-from .models import *
+from .models import Post
 
 
 class PostForm(forms.ModelForm):
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,9 @@
+from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.shortcuts import render, redirect,  get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+
 from .forms import PostForm
 from .models import Post, Group, User
-from django.contrib.auth.decorators import login_required
 
 
 def index(request):
